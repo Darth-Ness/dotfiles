6bd22934919cc311133c617e4e2f0f6c925636dc
Darth-Ness <75047141+Darth-Ness@users.noreply.github.com>
2025-03-19 23:20:41 +0000

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    fish_vi_key_bindings
+
+    alias cpd="echo (pwd) | xclip -selection clipboard"
+    alias cpff="xclip -sel clip <"
+    alias calc="bash ~/Documents/programming/scripts/calc"
+    alias spell="bash ~/Documents/programming/scripts/spell"
+    alias note="bash ~/Documents/programming/scripts/note"
+end
