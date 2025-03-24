e9d278413ca974a253c975d8bf3d1ac45cb48739
Sky <reeuuthdc80j@opayq.net>
2025-03-24 14:26:45 -0400

    Add update script

---
+++ update.sh
@@ -0,0 +1,9 @@
+# This is a script that when run updates the files in this repo with the files on my system
+
+cp -r ~/.config/polybar/* polybar/
+cp -r ~/.config/spicetify/* spicetify
+
+cat ~/.config/fish/config.fish > config.fish
+cat ~/.config/i3/config > i3config
+cat ~/.config/picom > picom.conf
+cat ~/.config/nvim/init.vim > vimrc
