3dc490fef0991e677e5e5235693e4c1d2676de79
Sky <reeuuthdc80j@opayq.net>
2025-05-09 18:45:12 -0400

    Make update script include bash scripts

--- update.sh
+++ update.sh
@@ -2,8 +2,10 @@
 
 cp -r ~/.config/polybar/* polybar/
 cp -r ~/.config/spicetify/* spicetify
+cp -r ../scripts/* scripts/
 
 cat ~/.config/fish/config.fish > config.fish
 cat ~/.config/i3/config > i3config
 cat ~/.config/picom > picom.conf
 cat ~/.config/nvim/init.vim > vimrc
+
