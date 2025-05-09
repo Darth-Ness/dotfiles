324cdf093b8f32ecadacf8907c4d578049b0fe8d
Sky <reeuuthdc80j@opayq.net>
2025-05-09 18:52:15 -0400

    Fix battery colors

--- polybar/battery.sh
+++ polybar/battery.sh
@@ -1,9 +1,14 @@
 letter_result=$(gdbus call --session --dest org.kde.kdeconnect --object-path /modules/kdeconnect/devices/67e758a7540f5e15/battery --method org.freedesktop.DBus.Properties.Get org.kde.kdeconnect.device.battery charge | awk -F'[<>]' '{for(i=2;i<=NF;i+=2) print $i}')%
-result=${letter_result%?}
-if [[ $result -gt 80 ]]; then
-    echo "%{F#A3BE8C} $letter_result %{F-}"
-elif [[ $result -lt 20 ]]; then
-    echo "%{F#BF616A} $letter_result %{F-}"
-else
-    echo $letter_result 
+if [[ $letter_result == "%" ]]; then
+    echo
+   else 
+    result=${letter_result%?}
+    if [[ $result -gt 80 ]]; then
+        echo "%{F#A3BE8C} $letter_result %{F-}"
+    elif [[ $result -lt 20 ]]; then
+        echo "%{F#BF616A} $letter_result %{F-}"
+    else
+        echo $letter_result 
+    fi
+
 fi
